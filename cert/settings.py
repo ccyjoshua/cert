bd99a75d2c6721372f50e6e488cbfe48cc5813d5
ccyjoshua <ccyjoshua@gmail.com>
2017-08-29 21:35:53 -0700

    Allow host

--- cert/settings.py
+++ cert/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5_3f*5^o(#p3*r8fqq=))l#^wm+^2noksxaw_s#0o4okij0)*e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
